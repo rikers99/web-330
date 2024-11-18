511146330ba88bd91f1b35ce3e54021669f2073f
Steve Culmer <culmersteve11@gmail.com>
2024-11-18 14:16:47 -0500

    update

---
+++ week-3/chapter/js09b.js
@@ -0,0 +1,11 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 9
+      Chapter case
+
+      Eating Well in Season
+      Author: Steve Culmer
+      Date:   6 nov 24
+
+      Filename: js09b.js
+ */
